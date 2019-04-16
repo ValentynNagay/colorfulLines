93a81ec9f7ae9f4626d1e80e276451b8fbcba7ce
Valentyn Nagai <nagayvalentyn@gmail.com>
2019-04-16 10:17:39 +0300

    Refactors index.js to use a better approach

--- js/index.js
+++ js/index.js
@@ -1,103 +1,44 @@
-var params = {
+const data = {
     lines: [
-        {
-            background: 'red',
-            updateTime: 1000,
-            elements: [
-                {
-                    background: getRandomColor(),
-                    width: 25
-                },
-                {
-                    background: getRandomColor(),
-                    width: 50
-                },
-                {
-                    background: getRandomColor(),
-                    width: 25
-                }
-            ]
-        },
-        {
-            background: 'yellow',
-            updateTime: 1000,
-            elements: [
-                {
-                    background: getRandomColor(),
-                    width: 25
-                },
-                {
-                    background: getRandomColor(),
-                    width: 50
-                },
-                {
-                    background: getRandomColor(),
-                    width: 25
-                }
-            ]
-        },
-        {
-            background: 'green',
-            updateTime: 1000,
-            elements: [{
-                background: getRandomColor(),
-                width: 25
-            },
-                {
-                    background: getRandomColor(),
-                    width: 50
-                },
-                {
-                    background: getRandomColor(),
-                    width: 25
-                }]
-        }]
-};
+      {
+        background: "#00f",
+        updateTime: 1000,
+        elements: [
+          {
+            background: "#00f",
+            width: 25
+          },
+          {
+            background: "#f00",
+            width: 50
+          },
+          {
+            background: "#0f0",
+            width: 25
+          }
+        ]
+      },
+      {
+        background: "#000",
+        updateTime: 700,
+        elements: [
+          {
+            background: "#000",
+            width: 70
+          },
+          {
+            background: "palevioletred",
+            width: 30
+          }
+        ]
+      },
+      {
+        background: "#0f0"
+      }
+    ]
+  };
 
-params.lines.forEach(({ background, elements }) => {
-    const lineToAdd = document.createElement('div');
-    for (let index = 0; index < 3; index++) {
-        lineToAdd.appendChild(document.createElement('div'));
-    }
-
-    const firstColumn = lineToAdd.getElementsByTagName('div')[0];
-    firstColumn.setAttribute('style', `
-            float: left;
-            width: ${elements[0].width}%;
-            background-color: ${elements[0].background};
-            height: ${window.innerHeight / params.lines.length}px
-        `);
-       
-
-    const secondColumn = lineToAdd.getElementsByTagName('div')[1];
-    secondColumn.setAttribute('style', `
-            float: left;
-            width: ${elements[1].width}%;
-            background-color: ${elements[1].background};
-            height: ${window.innerHeight / params.lines.length}px
-        `);
-
-    const thirdColumn = lineToAdd.getElementsByTagName('div')[2];
-    thirdColumn.setAttribute('style', `
-            float: left;
-            width: ${elements[2].width}%;
-            background-color: ${elements[2].background};
-            height: ${window.innerHeight / params.lines.length}px
-        `);
-
-    document.body.appendChild(lineToAdd)
-});
-
-window.onload = setupRefresh;
-
-function setupRefresh() {
-    setTimeout("refreshPage();", 3000); // milliseconds
-}
-function refreshPage() {
-    window.location = location.href;
-}
-
-function getRandomColor(updateTime) {
+function getRandomColor() {
     var letters = '0123456789ABCDEF';
     var color = '#';
     for (var i = 0; i < 6; i++) {
@@ -105,3 +46,48 @@ function getRandomColor(updateTime) {
     }
     return color;
 }
+  
+  const setRandomColor = (element, interval = 500) => {
+    setInterval(() => {
+      element.style.backgroundColor = getRandomColor();
+    }, interval);
+  };
+  
+  data.lines.forEach(({ elements, background, updateTime }) => {
+    const lineToAdd = document.createElement("div");
+  
+    lineToAdd.setAttribute(
+      "style",
+      `
+      display: flex;
+      flex-flow: row nowrap;
+      justify-content: flex-start;
+      align-items: center;
+      width: 100%;
+      height: ${window.innerHeight / data.lines.length}px;
+      background-color: ${background};
+      transition: background-color .3s ease-in-out;
+    `
+    );
+  
+    if (elements) {
+      elements.forEach(({ background: subDivBackground, width }) => {
+        const subDiv = document.createElement("div");
+        subDiv.setAttribute(
+          "style",
+          `
+          height: 100%;
+          width: ${width}%;
+          background-color: ${subDivBackground};
+          transition: background-color .3s ease-in-out;
+        `
+        );
+  
+        lineToAdd.appendChild(subDiv);
+        setRandomColor(subDiv, updateTime);
+      });
+    }
+  
+    document.body.appendChild(lineToAdd);
+    setRandomColor(lineToAdd, updateTime);
+  });
